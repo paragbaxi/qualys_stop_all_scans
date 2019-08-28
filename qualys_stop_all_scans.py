e70d1595099d15ca81af411590d3d9b28af6dacc
Parag Baxi <ghee22@gmail.com>
2019-08-27 23:12:59 -0400

    Update qualys_stop_all_scans.py

--- qualys_stop_all_scans.py
+++ qualys_stop_all_scans.py
@@ -5,7 +5,10 @@ q = qualysapi.connect()
 response = q.request('/api/2.0/fo/scan/',{'action': 'list','state':'Running'})
 root = objectify.fromstring(bytes(response, encoding='utf-8'))
 # Iterate scans and store scan references.
-for scan in root.RESPONSE.SCAN_LIST.SCAN:
-    print(f'Canceling {scan.TITLE.text}: {scan.REF.text}...')
-    response = q.request('/api/2.0/fo/scan/',{'action': 'cancel','scan_ref': scan.REF.text})
-    print(response)
+try:
+    for scan in root.RESPONSE.SCAN_LIST.SCAN:
+        print(f'Canceling {scan.TITLE.text}: {scan.REF.text}...')
+        response = q.request('/api/2.0/fo/scan/',{'action': 'cancel','scan_ref': scan.REF.text})
+        print(response)
+except AttributeError:
+    print('No scans running')
